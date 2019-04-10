d3f65c97b4fed3d01a5e3c40a39570456acf9973
juk18002 <juk18002@byui.edu>
2019-04-10 18:44:25 -0500

    updates

---
+++ assignments/lesson-14/scripts/temple.js
@@ -0,0 +1,78 @@
+//get the card container div
+var templeDiv = document.getElementById("temple-div");
+var templeList = document.getElementById("templelist");
+
+//get the json data
+//var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json';
+var requestURL = 'https://juk18002.github.io/assignments/lesson-14/data/temples.json';
+
+var request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+
+request.send();
+
+request.onload = function () {
+    var varJSON = request.response;
+
+    createTemples(varJSON);
+}
+
+function createTemples(textJSON) {
+
+    var templesJson = textJSON.temples;
+var ul = document.createElement('ul');
+ul.setAttribute('id', 'templeNav');
+templeList.appendChild(ul);
+
+   for (var i = 0; i < templesJson.length; i++) {
+        var objJSON = templesJson[i];
+
+        //set up links to individual temples
+        var tempDiv = document.createElement('div');
+        tempDiv.className = 'temple';
+        tempDiv.id = objJSON.city;
+        templeDiv.appendChild(tempDiv);
+        var linkTemp = "<a href='#" + tempDiv.id +">"+objJSON.name+"<a>";
+        var liTemple = document.createElement('li');
+        liTemple.innerHTML=linkTemp;
+        ul.appendChild(liTemple);
+
+        //temple name
+        var h1TempleName = document.createElement('h1');
+        h1TempleName.textContent = objJSON.name;
+        tempDiv.appendChild(h1TempleName);
+
+        //Contact Div
+        var tempDiv = document.createElement('div');
+        contactDiv.className = 'temple-contact';
+        tempDiv.appendChild(contactDiv);
+        var pAddress = document.createElement('p');
+        pAddress.textContent = objJSON.streetAddress.toString();
+        var pCityZip = document.createElement('p');
+        pCityZip.textContent = objJSON.city.toString() + ' ' + objJSON.state.toString() + ', ' + objJSON.postal_code.toString();
+        var pCountry = document.createElement('p');
+        pCountry.textContent = objJSON.country.toString();
+        var pPhone = document.createElement('p');
+        pPhone.textContent = objJSON.telephone.toString();
+        var aURL = document.createElement('a');
+        aURL.setAttribute('href', objJSON.ldsTempleURL );    
+        aURL.innerHTML='Church website'
+        contactDiv.appendChild(pAddress);
+        contactDiv.appendChild(pCityZip);
+        contactDiv.appendChild(pCountry);
+        contactDiv.appendChild(pPhone);
+        contactDiv.appendChild(aURL);
+        
+
+       //Services
+
+       //ordinance schedule
+
+       //closure schedule
+
+       //history
+       
+        
+    }
+}
